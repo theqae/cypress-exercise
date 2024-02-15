22e9520e5498e23df0a326b043813a065b00004f
Anthony <157030428+theqae@users.noreply.github.com>
2024-02-15 16:06:43 -0500

    added asserions for Create Account

--- cypress/e2e/tests/automationExercise.cy.js
+++ cypress/e2e/tests/automationExercise.cy.js
@@ -15,49 +15,45 @@ describe ('automation-exercise', () => {
   })
 
   it('register the user', () => {
-    //Creating variables so we only need to change inputs in one place
-    //let fName = 'Boden'
+    // Creating variables so we only need to change inputs in one place
     let randomFName = faker.person.firstName()
-    //let email = 'qaTest@example.com'
     let randomEmail = faker.internet.email()
     let password = 'Password1'
-    //let addressFName = 'Courtney'
     let randomAddressFName = faker.person.firstName()
-    //let addressLName = 'Coffee'
     let randomAddressLName = faker.person.lastName()
     
-    //Click Signup/Login (this also verifies the button exists)
+    // Click Signup/Login (this also verifies the button exists)
     cy.contains('Signup / Login').click()
 
-    //The new page should contain "New User Signup!" 
+    // The new page should contain "New User Signup!" 
     cy.get('.signup-form').contains('New User Signup!')
 
-    //Enter name and email then assert that the values were entered
+    // Enter name and email then assert that the values were entered
     cy.get('[data-qa="signup-name"]').type(randomFName).should('have.value', randomFName)
     cy.get('[data-qa="signup-email"]').type(randomEmail).should('have.value', randomEmail)
 
-    //Click Signup (this also verifies the button exists)
+    // Click Signup (this also verifies the button exists)
     cy.get('[data-qa="signup-button"]').click()
 
-    //Verify "Enter Account Information" exists
+    // Verify "Enter Account Information" exists
     cy.contains('Enter Account Information')
 
-    //Fill Account information while asserting the expected values
-    cy.get('#id_gender1').click() //Used the element's ID because the radio buttons share the same data-qa tag
+    // Fill Account information while asserting the expected values
+    cy.get('#id_gender1').click() // Used the element's ID because the radio buttons share the same data-qa tag
     cy.get('[data-qa="name"]').should('have.value', randomFName)
     cy.get('[data-qa="email"]').should('have.value', randomEmail)
     cy.get('[data-qa="password"]').type(password).should('have.value', password)
-    cy.get('[data-qa="days"]').select('2').should('have.value', '2') //Date of Birth - Day
-    cy.get('[data-qa="months"]').select('2').should('have.value','2') //Date of Birth - Month
+    cy.get('[data-qa="days"]').select('2').should('have.value', '2') // Date of Birth - Day
+    cy.get('[data-qa="months"]').select('2').should('have.value','2') // Date of Birth - Month
     cy.get('[data-qa="years"]').select('1980').should('have.value','1980') // Date of Birth - Year 
 
-    //Check 'Sign Up for Our News Letter'
+    // Check 'Sign Up for Our News Letter'
     cy.get('#newsletter').check() //Used the element's ID because no qa-data tag exists
     
-    //Check 'Receive special offers from our partners!'
+    // Check 'Receive special offers from our partners!'
     cy.get('#optin').check() //Used the element's ID because no qa-data tag exists
 
-    //Fill Address information
+    // Fill Address information
     cy.get('[data-qa="first_name"]').type(randomAddressFName).should('have.value', randomAddressFName)
     cy.get('[data-qa="last_name"]').type(randomAddressLName).should('have.value', randomAddressLName)
     cy.get('[data-qa="company"]').type('TheQAEngineer').should('have.value', 'TheQAEngineer')
@@ -69,7 +65,9 @@ describe ('automation-exercise', () => {
     cy.get('[data-qa="zipcode"]').type('55555').should('have.value', '55555')
     cy.get('[data-qa="mobile_number"]').type('(555) 565-6755').should('have.value', '(555) 565-6755')
     
-    //Create Account
+    // Create Account
     cy.get('[data-qa="create-account"]').click()
+    cy.url().should('eq', 'https://www.automationexercise.com/account_created') // Verify we are on the account created page
+    cy.contains('Account Created!') // Verify 'Account Created!' appears on screen
   })
 })
