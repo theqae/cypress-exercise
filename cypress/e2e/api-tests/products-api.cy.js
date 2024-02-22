ce65384a70f69e70f2884d54a2e0a5ca5d78ad68
Anthony <157030428+theqae@users.noreply.github.com>
2024-02-22 16:07:23 -0500

    completed first few API calls with assertions

--- cypress/e2e/api-tests/products-api.cy.js
+++ cypress/e2e/api-tests/products-api.cy.js
@@ -1,19 +1,27 @@
 import { faker } from '@faker-js/faker';
 
-context("GET /productsList", () => {
-  it("gets a list of products", () => {
-    cy.request("GET", "https://automationexercise.com/api/productsList").then((response) => {
+describe ('Products List API', () => {
+  context("GET /productsList", () => {
+  
+    it("gets a list of products", () => {
+
+      cy.request("GET", "https://automationexercise.com/api/productsList").then((response) => {
       expect(response.status).to.eq(200)
+      expect(JSON.parse(response.body).responseCode).to.eq(200) //This syntax is required for parsing the JSON because the Response Header include 'content-type text/html; charset=utf-8' and not 'application/json'
       expect(response.body).length.to.be.greaterThan(1)
+
+      })
     })
   })
-})
-context("POST /productsList", () => {
-  it("POST /productsList", () => {
-    cy.request("POST", "https://automationexercise.com/api/productsList").then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body).to.eq('responseCode": 405, "message": "This request method is not supported.')
-      expect(response.body).to.have.property('message', 'This request method is not supported.')
+
+  context("POST /productsList", () => {
+    it("POST /productsList", () => {
+      cy.request("POST", "https://automationexercise.com/api/productsList").then((response) => {
+        expect(response.status).to.eq(200)
+        expect(JSON.parse(response.body).responseCode).to.eq(405)
+        //expect(response.body).to.have.property('message')
+        expect(JSON.parse(response.body).message).to.eq('This request method is not supported.')
+      })
     })
   })
 })
