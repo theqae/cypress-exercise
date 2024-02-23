d7686aa93471c5786acc7308a004aedc2787b191
Anthony <157030428+theqae@users.noreply.github.com>
2024-02-23 17:07:52 -0500

    added more test cases and refactored a bit

--- cypress/e2e/api-tests/products-api.cy.js
+++ cypress/e2e/api-tests/products-api.cy.js
@@ -1,29 +1,51 @@
 import { faker } from '@faker-js/faker';
 
 describe ('Products List API', () => {
-  context("GET /productsList", () => {
   
-    it("gets a list of products", () => {
+    it("GET Products List", () => {
       cy.request("GET", "https://automationexercise.com/api/productsList").then((response) => {
         // Created a variable for the parsed JSON since we'll use it often  
         let parsedBody = (JSON.parse(response.body))
-
+        // Assertions
         expect(response.status).to.eq(200)
         expect(parsedBody.responseCode).to.eq(200) // This syntax is required for parsing the JSON because the Response Header include 'content-type text/html; charset=utf-8' and not 'application/json'
         expect(parsedBody.products.length).to.eq(34) // Check that we received all products in the Product List
 
       })
     })
-  })
 
-  context("POST /productsList", () => {
-    it("POST /productsList", () => {
+
+    it("POST Products List", () => {
       cy.request("POST", "https://automationexercise.com/api/productsList").then((response) => {
+        // Created a variable for the parsed JSON since we'll use it often  
+        let parsedBody = (JSON.parse(response.body))
+        // Assertions
         expect(response.status).to.eq(200)
-        expect(JSON.parse(response.body).responseCode).to.eq(405)
-        //expect(response.body).to.have.property('message')
-        expect(JSON.parse(response.body).message).to.eq('This request method is not supported.')
+        expect(parsedBody.responseCode).to.eq(405)
+        expect(parsedBody.message).to.eq('This request method is not supported.')
       })
     })
   })
-})
+  context("Brands List API", () => {
+    it("GET Brands List", () => {
+      cy.request("GET", "https://automationexercise.com/api/brandsList").then((response) => {
+        // Created a variable for the parsed JSON since we'll use it often  
+        let parsedBody = (JSON.parse(response.body))
+        // Assertions
+        expect(response.status).to.eq(200)
+        expect(parsedBody.responseCode).to.eq(200)
+        expect(parsedBody.brands.length).to.eq(34) // Check that we received all brands in the Brands List
+      })
+    })
+
+    it("PUT Brands List", () => {
+      cy.request("PUT", "https://automationexercise.com/api/brandsList").then((response) => {
+        // Created a variable for the parsed JSON since we'll use it often  
+        let parsedBody = (JSON.parse(response.body))
+        // Assertions
+        expect(response.status).to.eq(200)
+        expect(parsedBody.responseCode).to.eq(405)
+        expect(parsedBody.message).to.eq('This request method is not supported.')
+      })
+    })
+  })
