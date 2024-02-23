79dcf009dc8d0318959cef3332a60a3de2225c5e
Anthony <157030428+theqae@users.noreply.github.com>
2024-02-23 14:20:22 -0500

    refining assertions in the GET

--- cypress/e2e/api-tests/products-api.cy.js
+++ cypress/e2e/api-tests/products-api.cy.js
@@ -4,11 +4,13 @@ describe ('Products List API', () => {
   context("GET /productsList", () => {
   
     it("gets a list of products", () => {
-
       cy.request("GET", "https://automationexercise.com/api/productsList").then((response) => {
-      expect(response.status).to.eq(200)
-      expect(JSON.parse(response.body).responseCode).to.eq(200) //This syntax is required for parsing the JSON because the Response Header include 'content-type text/html; charset=utf-8' and not 'application/json'
-      expect(response.body).length.to.be.greaterThan(1)
+        // Created a variable for the parsed JSON since we'll use it often  
+        let parsedBody = (JSON.parse(response.body))
+
+        expect(response.status).to.eq(200)
+        expect(parsedBody.responseCode).to.eq(200) // This syntax is required for parsing the JSON because the Response Header include 'content-type text/html; charset=utf-8' and not 'application/json'
+        expect(parsedBody.products.length).to.eq(34) // Check that we received all products in the Product List
 
       })
     })
